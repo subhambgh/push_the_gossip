fcd2e387a3ce5742c44a0b48b34c0e9343e68c67
PranavPuranik <pranavpuranik10@gmail.com>
2019-09-18 21:40:37 -0400

    First Commit

---
+++ lib/push_the_gossips.ex
@@ -0,0 +1,18 @@
+defmodule PushTheGossips do
+  @moduledoc """
+  Documentation for PushTheGossips.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PushTheGossips.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/push_the_gossips_test.exs
@@ -0,0 +1,8 @@
+defmodule PushTheGossipsTest do
+  use ExUnit.Case
+  doctest PushTheGossips
+
+  test "greets the world" do
+    assert PushTheGossips.hello() == :world
+  end
+end
